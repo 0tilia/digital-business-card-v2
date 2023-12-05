1b7eb0fec9055c426e9812f3d87bcafe5b9a81dd
Otilia Toma <otilia@192-168-0-132.rdsnet.ro>
2023-12-05 19:31:47 +0200

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <card className="background--footer">
+            <img src='src/images/Icons-Background.png' className='background--footer' />
+            <div>
+            <a href='https://github.com/0tilia'>
+             <img src='/images/GitHub--Icon.png'className='github--icon' />
+             </a>
+            </div>
+        </card>    
+    )
+}
